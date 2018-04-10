f6848cffc117aa9412589135f3cf5f8f15aa6a1b
krzychna <kkrzys17@gmail.com>
2018-04-10 13:26:47 +0200

    Zadanie udemy

--- Test/src/Test/Butelka.java
+++ Test/src/Test/Butelka.java
@@ -1,6 +1,6 @@
 package Test;
 public class Butelka{
-
+    private static int pojemnosc = 10;
     private double ilelitrow;
     Butelka(double ilelitrow){
         this.ilelitrow = ilelitrow;
@@ -9,14 +9,31 @@ public class Butelka{
         return this.ilelitrow;
     }
     void wlej (double ilosc){
-        this.ilelitrow +=ilosc;
+        if (this.getIlelitrow()+ilosc>pojemnosc)
+        {
+            System.out.println("przeleje sie");
+            return;
+        }
+        else
+        {
+            this.ilelitrow +=ilosc;
+        }
+
     }
+
     void wylej (double ilosc){
-        this.ilelitrow -=ilosc;
+        if (ilosc > this.getIlelitrow()){
+            System.out.println("nie mozna wylac wiecej niz jest");
+            return;
+        }
+        else {
+            this.ilelitrow -=ilosc;
+        }
     }
     void przelej (double ilosc, Butelka gdzie){
-        gdzie.wlej(ilosc);
         this.wylej(ilosc);
+        gdzie.wlej(ilosc);
+
     }
 
 
@@ -26,8 +43,11 @@ public class Butelka{
         Butelka[] butelka = new Butelka[rozmiar];
         for (int i =0;i<rozmiar;i++){
             butelka[i] = new Butelka((i+1)*Math.sin((i+1)/50.0));
+            if (i>20&&i<45){
+                butelka[i].przelej(Math.sin((i+1)/50.0),butelka[11]);
+            }
         }
-        for (int i =0; i<rozmiar;i++) {
+        for (int i =0;i<rozmiar;i++) {
             System.out.println(butelka[i].getIlelitrow());
         }
     }
